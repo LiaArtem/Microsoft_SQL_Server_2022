fc7e973f613dfaf7ac4dfd4768b0517543bc2923
Admin <metrix@ukr.net>
2022-10-09 21:26:04 +0300

    Fix script

--- procedure/interface/insert_kurs.sql
+++ procedure/interface/insert_kurs.sql
@@ -8,7 +8,7 @@ BEGIN
    SELECT @m_curr_code = c.CODE FROM CURRENCY c WHERE c.SHORT_NAME = @p_curr_code;
 
    INSERT INTO KURS (KURS_DATE, CURRENCY_CODE, RATE, FORC)
-       SELECT @p_curs_date, @m_curr_code, @p_rate, 1
-       WHERE NOT EXISTS (SELECT 1 FROM KURS c where c.KURS_DATE = @p_curs_date and c.CURRENCY_CODE = @m_curr_code);     
+       SELECT CONVERT(date, @p_curs_date, 23), @m_curr_code, @p_rate, 1
+       WHERE NOT EXISTS (SELECT 1 FROM KURS c where c.KURS_DATE = CONVERT(date, @p_curs_date, 23) and c.CURRENCY_CODE = @m_curr_code);     
 RETURN 0
 END
